4cf647432c98cd54f56dd5394b14fa353ae352a2
Marko <Marko@DESKTOP-685L8RG>
2022-03-27 22:41:42 +0200

    Finito

---
+++ EsDnevnik/Osoba.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace EsDnevnik
+{
+    partial class Osoba
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_ime = new System.Windows.Forms.TextBox();
+            this.txt_prezime = new System.Windows.Forms.TextBox();
+            this.txt_adresa = new System.Windows.Forms.TextBox();
+            this.txt_jmbg = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.txt_uloga = new System.Windows.Forms.TextBox();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.lbl_ime = new System.Windows.Forms.Label();
+            this.lbl_prezime = new System.Windows.Forms.Label();
+            this.lbl_adresa = new System.Windows.Forms.Label();
+            this.lbl_jmbg = new System.Windows.Forms.Label();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.lbl_uloga = new System.Windows.Forms.Label();
+            this.btn_first = new System.Windows.Forms.Button();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_last = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_change = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_id
+            // 
+            this.txt_id.Enabled = false;
+            this.txt_id.Location = new System.Drawing.Point(314, 30);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(156, 23);
+            this.txt_id.TabIndex = 0;
+            // 
+            // txt_ime
+            // 
+            this.txt_ime.Location = new System.Drawing.Point(314, 59);
+            this.txt_ime.Name = "txt_ime";
+            this.txt_ime.Size = new System.Drawing.Size(156, 23);
+            this.txt_ime.TabIndex = 0;
+            // 
+            // txt_prezime
+            // 
+            this.txt_prezime.Location = new System.Drawing.Point(314, 88);
+            this.txt_prezime.Name = "txt_prezime";
+            this.txt_prezime.Size = new System.Drawing.Size(156, 23);
+            this.txt_prezime.TabIndex = 0;
+            // 
+            // txt_adresa
+            // 
+            this.txt_adresa.Location = new System.Drawing.Point(314, 117);
+            this.txt_adresa.Name = "txt_adresa";
+            this.txt_adresa.Size = new System.Drawing.Size(156, 23);
+            this.txt_adresa.TabIndex = 0;
+            // 
+            // txt_jmbg
+            // 
+            this.txt_jmbg.Location = new System.Drawing.Point(314, 146);
+            this.txt_jmbg.Name = "txt_jmbg";
+            this.txt_jmbg.Size = new System.Drawing.Size(156, 23);
+            this.txt_jmbg.TabIndex = 0;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(314, 175);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(156, 23);
+            this.txt_email.TabIndex = 0;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(314, 204);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(156, 23);
+            this.txt_pass.TabIndex = 0;
+            // 
+            // txt_uloga
+            // 
+            this.txt_uloga.Location = new System.Drawing.Point(314, 233);
+            this.txt_uloga.Name = "txt_uloga";
+            this.txt_uloga.Size = new System.Drawing.Size(156, 23);
+            this.txt_uloga.TabIndex = 0;
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Location = new System.Drawing.Point(230, 33);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(18, 15);
+            this.lbl_id.TabIndex = 1;
+            this.lbl_id.Text = "ID";
+            // 
+            // lbl_ime
+            // 
+            this.lbl_ime.AutoSize = true;
+            this.lbl_ime.Location = new System.Drawing.Point(230, 62);
+            this.lbl_ime.Name = "lbl_ime";
+            this.lbl_ime.Size = new System.Drawing.Size(27, 15);
+            this.lbl_ime.TabIndex = 1;
+            this.lbl_ime.Text = "Ime";
+            // 
+            // lbl_prezime
+            // 
+            this.lbl_prezime.AutoSize = true;
+            this.lbl_prezime.Location = new System.Drawing.Point(230, 91);
+            this.lbl_prezime.Name = "lbl_prezime";
+            this.lbl_prezime.Size = new System.Drawing.Size(49, 15);
+            this.lbl_prezime.TabIndex = 1;
+            this.lbl_prezime.Text = "Prezime";
+            // 
+            // lbl_adresa
+            // 
+            this.lbl_adresa.AutoSize = true;
+            this.lbl_adresa.Location = new System.Drawing.Point(230, 120);
+            this.lbl_adresa.Name = "lbl_adresa";
+            this.lbl_adresa.Size = new System.Drawing.Size(43, 15);
+            this.lbl_adresa.TabIndex = 1;
+            this.lbl_adresa.Text = "Adresa";
+            // 
+            // lbl_jmbg
+            // 
+            this.lbl_jmbg.AutoSize = true;
+            this.lbl_jmbg.Location = new System.Drawing.Point(230, 149);
+            this.lbl_jmbg.Name = "lbl_jmbg";
+            this.lbl_jmbg.Size = new System.Drawing.Size(37, 15);
+            this.lbl_jmbg.TabIndex = 1;
+            this.lbl_jmbg.Text = "JMBG";
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Location = new System.Drawing.Point(230, 178);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(41, 15);
+            this.lbl_email.TabIndex = 1;
+            this.lbl_email.Text = "E-mail";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Location = new System.Drawing.Point(230, 207);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(57, 15);
+            this.lbl_pass.TabIndex = 1;
+            this.lbl_pass.Text = "Password";
+            // 
+            // lbl_uloga
+            // 
+            this.lbl_uloga.AutoSize = true;
+            this.lbl_uloga.Location = new System.Drawing.Point(230, 236);
+            this.lbl_uloga.Name = "lbl_uloga";
+            this.lbl_uloga.Size = new System.Drawing.Size(38, 15);
+            this.lbl_uloga.TabIndex = 1;
+            this.lbl_uloga.Text = "Uloga";
+            // 
+            // btn_first
+            // 
+            this.btn_first.Location = new System.Drawing.Point(188, 290);
+            this.btn_first.Name = "btn_first";
+            this.btn_first.Size = new System.Drawing.Size(94, 29);
+            this.btn_first.TabIndex = 2;
+            this.btn_first.Text = "<<";
+            this.btn_first.UseVisualStyleBackColor = true;
+            this.btn_first.Click += new System.EventHandler(this.btn_first_Click);
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(288, 290);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(94, 29);
+            this.btn_prev.TabIndex = 2;
+            this.btn_prev.Text = "<";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(390, 290);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(94, 29);
+            this.btn_next.TabIndex = 2;
+            this.btn_next.Text = ">";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_last
+            // 
+            this.btn_last.Location = new System.Drawing.Point(490, 290);
+            this.btn_last.Name = "btn_last";
+            this.btn_last.Size = new System.Drawing.Size(94, 29);
+            this.btn_last.TabIndex = 2;
+            this.btn_last.Text = ">>";
+            this.btn_last.UseVisualStyleBackColor = true;
+            this.btn_last.Click += new System.EventHandler(this.btn_last_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(235, 325);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(94, 29);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_change
+            // 
+            this.btn_change.Location = new System.Drawing.Point(337, 325);
+            this.btn_change.Name = "btn_change";
+            this.btn_change.Size = new System.Drawing.Size(94, 29);
+            this.btn_change.TabIndex = 2;
+            this.btn_change.Text = "Change";
+            this.btn_change.UseVisualStyleBackColor = true;
+            this.btn_change.Click += new System.EventHandler(this.btn_change_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(437, 325);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(94, 29);
+            this.btn_delete.TabIndex = 2;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Osoba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_change);
+            this.Controls.Add(this.btn_last);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.btn_first);
+            this.Controls.Add(this.lbl_uloga);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_adresa);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.lbl_prezime);
+            this.Controls.Add(this.lbl_jmbg);
+            this.Controls.Add(this.lbl_ime);
+            this.Controls.Add(this.lbl_id);
+            this.Controls.Add(this.txt_uloga);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_jmbg);
+            this.Controls.Add(this.txt_adresa);
+            this.Controls.Add(this.txt_prezime);
+            this.Controls.Add(this.txt_ime);
+            this.Controls.Add(this.txt_id);
+            this.Name = "Osoba";
+            this.Text = "Osoba";
+            this.Load += new System.EventHandler(this.Osoba_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_ime;
+        private System.Windows.Forms.TextBox txt_prezime;
+        private System.Windows.Forms.TextBox txt_adresa;
+        private System.Windows.Forms.TextBox txt_jmbg;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.TextBox txt_uloga;
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.Label lbl_ime;
+        private System.Windows.Forms.Label lbl_prezime;
+        private System.Windows.Forms.Label lbl_adresa;
+        private System.Windows.Forms.Label lbl_jmbg;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.Label lbl_uloga;
+        private System.Windows.Forms.Button btn_first;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_last;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_change;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ EsDnevnik/Osoba.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EsDnevnik
+{
+    public partial class Osoba : Form
+    {
+        int rb = 0;
+        DataTable table;
+        public Osoba()
+        {
+            InitializeComponent();
+        }
+
+        private void load_Data()
+        {
+            SqlConnection connetion = Connection.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM osoba", connetion);
+            table = new DataTable();
+            adapter.Fill(table);
+        }
+
+        private void text_Load()
+        {
+            if(table.Rows.Count == 0)
+            {
+                txt_id.Text = "";
+                txt_ime.Text = "";
+                txt_prezime.Text = "";
+                txt_adresa.Text = "";
+                txt_jmbg.Text = "";
+                txt_email.Text = "";
+                txt_pass.Text = "";
+                txt_uloga.Text = "";
+                btn_delete.Enabled = false;
+            }
+            else
+            {
+                txt_id.Text = table.Rows[rb]["id"].ToString();
+                txt_ime.Text = table.Rows[rb]["ime"].ToString();
+                txt_prezime.Text = table.Rows[rb]["prezime"].ToString();
+                txt_adresa.Text = table.Rows[rb]["adresa"].ToString();
+                txt_jmbg.Text = table.Rows[rb]["jmbg"].ToString();
+                txt_email.Text = table.Rows[rb]["email"].ToString();
+                txt_pass.Text = table.Rows[rb]["pass"].ToString();
+                txt_uloga.Text = table.Rows[rb]["uloga"].ToString();
+                btn_delete.Enabled = true;
+            }
+
+            if(rb == 0)
+            {
+                btn_first.Enabled = false;
+                btn_prev.Enabled = false;
+            }
+            else
+            {
+                btn_first.Enabled = true;
+                btn_prev.Enabled = true;
+            }
+
+            if (rb == table.Rows.Count - 1)
+            {
+                btn_last.Enabled = false;
+                btn_next.Enabled = false;
+            }
+            else
+            {
+                btn_last.Enabled = true;
+                btn_next.Enabled = true;
+            }
+         
+        }
+
+        private void Osoba_Load(object sender, EventArgs e)
+        {
+            load_Data();
+            text_Load();
+        }
+
+        private void btn_first_Click(object sender, EventArgs e)
+        {
+            rb = 0;
+            text_Load();
+        }
+
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            rb--;
+            text_Load();
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            rb++;
+            text_Load();
+        }
+
+        private void btn_last_Click(object sender, EventArgs e)
+        {
+            rb = table.Rows.Count - 1;
+            text_Load();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            StringBuilder command = new StringBuilder("INSERT INTO osoba (ime, prezime, adresa, jmbg, email, pass, uloga) VALUES('");
+            command.Append(txt_ime.Text + "', '");
+            command.Append(txt_prezime.Text + "', '");
+            command.Append(txt_adresa.Text + "', '");
+            command.Append(txt_jmbg.Text + "', '");
+            command.Append(txt_email.Text + "', '");
+            command.Append(txt_pass.Text + "', '");
+            command.Append(txt_uloga.Text + "')");
+            SqlConnection connection = Connection.Connect();
+            SqlCommand cmd = new SqlCommand(command.ToString(), connection);
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception Greska)
+            {
+                MessageBox.Show(Greska.Message);
+            }
+            load_Data();
+            rb = table.Rows.Count - 1;
+            text_Load();
+        }
+
+        private void btn_change_Click(object sender, EventArgs e)
+        {
+            // UPDATE osoba SET ime = 'Marko', prezime = 'Peric', adresa = 'Studenac 8', jmbg = '123451352315', email = 'marko@gmail.com', pass = '121231', uloga = '1'
+            // WHERE id = 3
+            StringBuilder cmd = new StringBuilder("UPDATE Osoba SET ");
+            cmd.Append("ime = '" + txt_ime.Text + "', ");
+            cmd.Append("prezime = '" + txt_prezime.Text + "', ");
+            cmd.Append("adresa = '" + txt_adresa.Text + "', ");
+            cmd.Append("jmbg = '" + txt_jmbg.Text + "', ");
+            cmd.Append("email = '" + txt_email.Text + "', ");
+            cmd.Append("pass = '" + txt_pass.Text + "', ");
+            cmd.Append("uloga = '" + txt_uloga.Text + "' ");
+            cmd.Append("WHERE id = " + txt_id.Text);
+
+            SqlConnection connection = Connection.Connect();
+            SqlCommand command = new SqlCommand(cmd.ToString(), connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception Greska)
+            {
+                MessageBox.Show(Greska.Message);
+            }
+            load_Data();
+            rb = table.Rows.Count - 1;
+            text_Load();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            string command = "DELETE FROM Osoba WHERE id = " + txt_id.Text;
+
+            SqlConnection connection = Connection.Connect();
+            SqlCommand cmd = new SqlCommand(command.ToString(), connection);
+            Boolean brisano = false;
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                brisano = true;
+            }
+            catch (Exception Greska)
+            {
+                MessageBox.Show(Greska.Message);
+            }
+            if (brisano)
+            {
+                load_Data();
+                if (rb > 0)
+                    rb--;
+                text_Load();
+            }
+            load_Data();
+            rb = table.Rows.Count - 1;
+            text_Load();
+        }
+    }
+}
