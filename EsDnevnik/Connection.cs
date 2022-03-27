4b4436f4d6609c2f31b212688b3ec1a42f14b055
Marko <Marko@DESKTOP-685L8RG>
2022-03-27 21:29:42 +0200

    Konekcija i klasa connection

---
+++ EsDnevnik/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EsDnevnik
+{
+    class Connection
+    {
+        static public SqlConnection Connect()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["home"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
